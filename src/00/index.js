f097cd6b9beec52c7541957b8fadf76e8e61e59e
infantito <danielinfantito61@gmail.com>
2021-01-05 18:03:42 -0500

    feat: 💩 logic lives into a component

--- src/00/index.js
+++ src/00/index.js
@@ -1,17 +1,25 @@
-import { createMachine } from 'xstate';
-
-const elOutput = document.querySelector('#output');
+const elOutput = document.querySelector('#output')
 
 function output(object) {
-  elOutput.innerHTML = JSON.stringify(object, null, 2);
+  object.createdAt = Date.now()
+
+  elOutput.innerHTML = JSON.stringify(object, null, 2)
 }
 
-console.log('Welcome to the XState workshop!');
+console.log('Welcome to the XState workshop!')
 
 const user = {
-  name: 'David Khourshid',
-  company: 'Microsoft',
-  interests: ['piano', 'state machines'],
-};
+  name: 'Daniel Infante',
+  company: 'infantito',
+  interests: ['video games', 'action and drama series'],
+}
 
-output(user);
+const elButton = document.querySelector('#button')
+
+output(user)
+
+elButton.addEventListener('click', event => {
+  output(user)
+
+  event.target.setAttribute('disabled', 'true')
+})
