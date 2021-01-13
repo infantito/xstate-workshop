cbbed573a8aa8f9ed443f453d77ce3c33d5e8796
infantito <danielinfantito61@gmail.com>
2021-01-12 20:58:40 -0500

    feat: ✨  xstate actions

--- src/04/index.js
+++ src/04/index.js
@@ -3,31 +3,36 @@ import { createMachine, interpret } from 'xstate'
 const elBox = document.querySelector('#box')
 
 const setPoint = (context, event) => {
-  // Set the data-point attribute of `elBox`
-  // ...
+  elBox.dataset.point = `(${event.clientX}, ${event.clientY})`
 }
 
-const machine = createMachine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        mousedown: {
-          // Add your action here
-          // ...
-          target: 'dragging',
+const machine = createMachine(
+  {
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          mousedown: {
+            target: 'dragging',
+            actions: [setPoint],
+          },
+        },
+      },
+      dragging: {
+        on: {
+          mouseup: {
+            target: 'idle',
+          },
         },
       },
     },
-    dragging: {
-      on: {
-        mouseup: {
-          target: 'idle',
-        },
-      },
-    },
-  },
-})
+  }
+  /**
+   * It replaces the above actions
+   *
+   * { actions: [setPoint] }
+   */
+)
 
 const service = interpret(machine)
 
@@ -39,10 +44,6 @@ service.onTransition(state => {
 
 service.start()
 
-elBox.addEventListener('mousedown', event => {
-  service.send(event)
-})
+elBox.addEventListener('mousedown', service.send)
 
-elBox.addEventListener('mouseup', event => {
-  service.send(event)
-})
+elBox.addEventListener('mouseup', service.send)
