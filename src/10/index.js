113c51b251575537b7988e32ed6d02ea5ab013d9
infantito <danielinfantito61@gmail.com>
2021-01-20 22:13:01 -0500

    feat: ✨  history states

--- src/10/index.js
+++ src/10/index.js
@@ -10,15 +10,28 @@ const displayMachine = createMachine({
   states: {
     hidden: {
       on: {
-        TURN_ON: 'visible',
+        TURN_ON: 'visible.history',
       },
     },
     visible: {
+      initial: 'light',
       // Add hierarchical states for light/dark mode.
-      // ...
-
-      // Then, add a history state that remembers which mode we used.
-      // ...
+      states: {
+        light: {
+          on: {
+            SWITCH: 'dark',
+          },
+        },
+        dark: {
+          on: {
+            SWITCH: 'light',
+          },
+        },
+        // Then, add a history state that remembers which mode we used.
+        history: {
+          type: 'history',
+        },
+      },
       on: {
         TURN_OFF: 'hidden',
       },
@@ -31,11 +44,6 @@ const displayService = interpret(displayMachine)
     elApp.dataset.state = state.toStrings().join(' ')
   })
   .start()
-
-// Add event listeners for:
-// - clicking elOnButton (TURN_ON)
-// - clicking elOffButton (TURN_OFF)
-// - clicking elModeButton (SWITCH)
 
 elOnButton.addEventListener('click', () => {
   displayService.send('TURN_ON')
